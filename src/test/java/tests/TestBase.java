7d47fe53bfc42a875a4be781c2f9ac33bed5d151
Elena Klepova <elena.klyopova@gmail.com>
2023-10-14 23:02:00 +0300

    Homework Lesson №13 Jenkins Properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,6 +18,7 @@ public class TestBase {
     @BeforeAll
     public static void beforeAll() {
 
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "117.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
