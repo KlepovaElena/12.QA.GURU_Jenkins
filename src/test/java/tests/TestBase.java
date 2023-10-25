fcd3920796327b45d067992ef6d59b50a0a97504
Elena Klepova <elena.klyopova@gmail.com>
2023-10-25 21:12:38 +0300

    Homework Lesson №17 Owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,37 +1,26 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.ConfigReader;
+import config.ProjectConfiguration;
+import config.WebConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static java.lang.String.format;
 
 public class TestBase {
+
+    private static final WebConfig webConfig = ConfigReader.Instance.read();
 
     @BeforeAll
     public static void beforeAll() {
 
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("version", "100.0");
-        String webDriver = System.getProperty("webDriver", "https://user1:1234@selenoid.autotests.cloud/");
-        Configuration.remote = format("%swd/hub", webDriver);
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+        ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
+        projectConfiguration.webConfig();
     }
 
     @BeforeEach
